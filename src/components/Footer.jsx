054ddfa7c931dfc8029bcbdfd42ef07c15d2544d
PRIYESH ANAND <priyeshanand9@gmail.com>
2023-01-01 09:27:07 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Footer = () => {
+  return (<footer>
+    <div>
+        <h1>TechnoSolution</h1>
+        <p>all rights reserved</p>
+    </div>
+
+    <div>
+        <h5>Follow Us</h5>
+        <div>
+        <a href='https://www.facebook.com/priyesh.anand.180/' target={"blank"}>
+            Facebook
+        </a>
+        <a href='https://www.instagram.com/priyesh867/' target={"blank"}>
+            Instagram
+        </a>
+        <a href='https://github.com/Priyesh786' target ={"blank"}>
+            Github
+        </a>
+        </div>
+    </div>
+ </footer>
+ );
+}
+
+export default Footer
